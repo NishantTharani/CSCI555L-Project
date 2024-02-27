c1bc0d2560b61fd671cdf1261c9c4b388df27508
Nishant Tharani <nishant.tharani.work@gmail.com>
2024-02-27 12:14:04 -0800

    Get IP address

--- setup.sh
+++ setup.sh
@@ -2,11 +2,38 @@
 # Directory name that we will use
 DIRNAME=data
 
-# TODO is there some way to get the node name and save it here? Then we can run different instructions for master/worker/client
+# Get the hostname
+hostname=$(hostname)
 
-cd /
-sudo mkdir $DIRNAME
-cd $DIRNAME
+# Check if the hostname ends with .utah.cloudlab.us
+if [[ $hostname == *".utah.cloudlab.us" ]]; then
+  cluster="UTAH"
+else
+  cluster="UNDEFINED"
+fi
+
+# If cluster is UNDEFINED, print an error and exit
+if [[ $cluster == "UNDEFINED" ]]; then
+  echo "Error: Cluster is UNDEFINED"
+  exit 1
+fi
+
+# Try to get the IP address of the interface named enp1s0d1
+ip_address=$(ifconfig enp1s0d1 | grep 'inet ' | awk '{print $2}')
+
+# Check if the ip_address variable is empty
+if [[ -z $ip_address ]]; then
+  echo "Error: Interface enp1s0d1 does not have an IP address or does not exist"
+  exit 1
+fi
+
+# If the script reaches this point, the ip_address variable
+# should contain the IP address of the interface enp1s0d1
+echo "IP Address of enp1s0d1: $ip_address"
+
+
+sudo mkdir -p /$DIRNAME
+cd /$DIRNAME
 
 # Create a new filesystem using the remaining space on the system (boot) disk, at /DIRNAME
 # Create a new filesystem using the remaining space on the system (boot) disk, at /DIRNAME
@@ -15,4 +42,15 @@ cd $DIRNAME
 
 # Install java
 sudo apt-get update
+# Sleep for a bit to let apt-get get its act together
+sleep 2
 sudo apt-get install openjdk-8-jdk -y
+
+sudo mkdir -p /$DIRNAME/hadoop
+cd /$DIRNAME/hadoop
+
+# Download hadoop
+sudo wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
+sudo tar zvxf hadoop-3.3.6.tar.gz
+sudo rm hadoop-3.3.6.tar.gz
+
