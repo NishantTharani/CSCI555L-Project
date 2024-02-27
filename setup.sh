228b2850c31e8b00ea90de09cf6e5743ff058222
Nishant Tharani <nishant.tharani.work@gmail.com>
2024-02-26 22:39:23 -0800

    Start with the setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 # Variables
-# Directory name for the new filesystem to be at
-DIRNAME=mydata
+# Directory name that we will use
+DIRNAME=data
 
-# Create a new filesystem using the remaining space on the system (boot) disk, at /DIRNAME
+# TODO is there some way to get the node name and save it here? Then we can run different instructions for master/worker/client
+
 cd /
 sudo mkdir $DIRNAME
-sudo /usr/local/etc/emulab/mkextrafs.pl /$DIRNAME
+cd $DIRNAME
+
+# Create a new filesystem using the remaining space on the system (boot) disk, at /DIRNAME
+# Create a new filesystem using the remaining space on the system (boot) disk, at /DIRNAME
+# The below seems unnecessary for now as we seem to be getting 68G anyway even though the docs say we should only be getting 16G?
+# sudo /usr/local/etc/emulab/mkextrafs.pl /$DIRNAME
+
+# Install java
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk -y
