3915d739cd5d4bd62d6ce2ed537db25bf09bb0ca
Nishant Tharani <nishant.tharani.work@gmail.com>
2024-04-09 15:35:31 -0700

    Fix indexing bug

--- setup.sh
+++ setup.sh
@@ -174,7 +174,7 @@ sudo cp "$hadoop_workers_file" "$hadoop_workers_file.backup"
 # echo "$WORKER2_IP" | sudo tee -a "$hadoop_workers_file"
 # echo "$WORKER3_IP" | sudo tee -a "$hadoop_workers_file"
 # Start from 1 to exclude the MASTER_IP
-for ((i=1; i<=NUM_WORKERS; i++)); do
+for ((i=0; i<NUM_WORKERS; i++)); do
   ip="${WORKER_IP_LIST[i]}"
   if ((i == 1)); then
     echo "$ip" | sudo tee "$hadoop_workers_file"
